745e84ddf50fab3818e4f5bbbda4a4989856ddf5
Anze Mramor <anze.mramor@student.fmf.uni-lj.si>
2020-04-25 14:19:40 +0200

    začetek vizualizacije

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -37,13 +37,35 @@ slovar <- c("Belgium" = "Belgija",
 
 
 # Uvozimo zemljevid.
-zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip", "OB",
-                             pot.zemljevida="OB", encoding="Windows-1250")
-levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
-  { gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
-zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels=levels(obcine$obcina))
-zemljevid <- fortify(zemljevid)
+#zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip", "OB",
+#                             pot.zemljevida="OB", encoding="Windows-1250")
+#levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
+#  { gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
+#zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels=levels(obcine$obcina))
+#zemljevid <- fortify(zemljevid)
+
+#################################################################################
+
+
+graf1 <- ggplot(data=dostop_do_interneta, aes(x=leto, y=delez, col=drzava)) + geom_line()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Izračunamo povprečno velikost družine
-povprecja <- druzine %>% group_by(obcina) %>%
-  summarise(povprecje=sum(velikost.druzine * stevilo.druzin) / sum(stevilo.druzin))
+#povprecja <- druzine %>% group_by(obcina) %>%
+#  summarise(povprecje=sum(velikost.druzine * stevilo.druzin) / sum(stevilo.druzin))
