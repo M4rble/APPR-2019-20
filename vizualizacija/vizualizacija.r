10841983e2e39a3410e248c55633041f2caeb4bb
Anze Mramor <anze.mramor@student.fmf.uni-lj.si>
2020-04-26 17:41:15 +0200

    dodatni grafi v vizualizaciji

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -55,14 +55,40 @@ graf1 <- ggplot(data=dostop_do_interneta, aes(x=leto, y=delez, col=drzava)) + ge
          labs(fill = "Država")
 
 povprecni_delez_dostopa <- dostop_do_interneta %>% 
-      group_by(drzava) %>%
-      summarise(povprecje = mean(delez, na.rm = TRUE))
+                           group_by(drzava) %>%
+                           summarise(povprecje = mean(delez, na.rm = TRUE))
 povprecni_delez_dostopa$povprecje <- round(povprecni_delez_dostopa$povprecje, 2)
 
 graf1a <- ggplot(data=povprecni_delez_dostopa, aes(x=drzava, y=povprecje, fill=drzava)) + 
-          geom_col() + coord_cartesian(ylim = c(40, 100)) +
-          ylab('povprečje v %') + xlab('Države') + ggtitle('Povprečen delež dostopanja do interneta')
+          geom_col() + coord_cartesian(ylim = c(40, 100)) + ggtitle('Povprečen delež dostopanja do interneta') +
+          ylab('povprečje v %') + xlab('Države') + theme(axis.text.x = element_blank())
 
+najvecja_rast <- dostop_do_interneta %>%
+                 group_by(drzava) %>%
+                 summarise(razlika = (max(delez, na.rm = TRUE) - min(delez, na.rm = TRUE)))
+
+primerjava_rasti <- dostop_do_interneta %>% 
+                    filter(drzava %in% c("Islandija", "Nizozemska", "Norveška",
+                                         "Bolgarija", "Turčija", "Grčija")) 
+
+graf1b <- ggplot(data=primerjava_rasti, aes(x=leto, y=delez, col=drzava)) + geom_line() + 
+          ylab('delež v %') + ggtitle('Primerjava rasti držav z največjo in najmanjšo rastjo') +
+          scale_x_continuous(breaks = 1*2007:2019) +labs(fill = "Država")
+
+povprecen_delez_razloga <- razlogi_za_ne_dostopanje_do_interneta %>%
+                           group_by(razlog) %>%
+                           summarise(sum(delez, na.rm = TRUE) / 132)
+names(povprecen_delez_razloga)[names(povprecen_delez_razloga) == "sum(delez, na.rm = TRUE)/132"] <- "delez"
+povprecen_delez_razloga$delez <- round(povprecen_delez_razloga$delez, 2)
+
+razlogi_po_letih <- razlogi_za_ne_dostopanje_do_interneta %>%
+                    filter(razlog %in% c("nepotreben", "pomanjkanje spretnosti", "predraga oprema")) %>%
+                    group_by(leto, razlog) %>%
+                    summarise(sum(delez, na.rm = TRUE) / 33)
+names(razlogi_po_letih)[names(razlogi_po_letih) == "sum(delez, na.rm = TRUE)/33"] <- "delez"
+razlogi_po_letih$delez <- round(razlogi_po_letih$delez, 2)
+
+#graf2a <- ggplot(data=razlogi_po_letih, aes(x=))
 
 
 
